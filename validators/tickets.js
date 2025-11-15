b5c7c675aaa9e0f5005cee547ad128b0e2aa8767
Tom Kester <kes19002@byui.edu>
2025-11-15 10:51:38 -0700

    add extra validator

--- validators/tickets.js
+++ validators/tickets.js
@@ -2,10 +2,18 @@ const validateTicket = (ticket) => {
     console.log("Validator ticket: ", ticket)
   let valid = false;
   let error = "";
+
   if (!ticket.subject || !ticket.customerId || ticket.description) {
     return {
         valid: valid,
-        error: "Missing data. subject and customerId are both required."
+        error: "Missing data. subject, customerId, and descriptions are required."
+    }
+  }
+
+  if (ticket.priority != "urgent" && ticket.priority != "normal"){
+    return {
+        valid: valid,
+        error: "Choose a valid priority. urgent or normal"
     }
   }
 
